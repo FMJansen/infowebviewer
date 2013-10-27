7277f0b2454a0d758467e0ec4b12d97754ff257e
Floris Jansen <florismartijnjansen@gmail.com>
2013-10-27 21:19:27 +0100

    reduce server requests (not when weeknumber is changed)

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -24,41 +24,53 @@ String.prototype.replaceBoldWithCase = function(subStr, newStr) {
   });
 }
 
-function fetchRoosters(searchInput) {
-  if(searchInput.val().length > 2) {
-    var svalue = searchInput.val();
-    $.ajax({
-      type: "POST",
-      url: "/fetch/",
-      data: { 
-        value: svalue,
-        week: weeknummer
+var response;
+
+function fetchUsers(searchInput, callback) {
+  var svalue = searchInput.val();
+  $.ajax({
+    type: "POST",
+    url: "/fetch/",
+    data: { 
+      value: svalue,
+      week: weeknummer
+    }
+  }).done(function(back) {
+      response = back;
+      if(callback) {
+        callback(searchInput);
       }
-    })
-      .done(function(back) {
-        if(back['users'] === 'true') {
-          var toReplace = searchInput.val();
-          var reg = new RegExp(toReplace,"gi");
+  });
+}
 
-          $('ul#acs').html('');
+function processUsers(searchInput) {
+  if(searchInput.val().length > 2) {
+    if(response['users'] === 'true') {
+      var toReplace = searchInput.val();
+      var reg = new RegExp(toReplace,"gi");
 
-          for(var key in back) {
-            if(back.hasOwnProperty(key) && key !== 'users') {
-              var itemName = back[key]['name'].replaceBoldWithCase(toReplace, toReplace);
-              var itemUrl = '/' + weeknummer + '/' + back[key]['ref'] + '/' + back[key]['llnr'] + '/';
-              var listItem = '<li><a href="' + itemUrl + '">' + itemName + ' (' + back[key]['llnr'] + ', ' + back[key]['group'] + ')</a></li>';
-              $('ul#acs').append(listItem);
-            }
-          }
+      $('ul#acs').html('');
 
-          $('ul#acs').children(':first').addClass('selected');
-        } else {
-          $('ul#acs').html('<li class="noresult"><i>Er zijn geen resultaten voor de naam waar je naar zoekt.</i></li>');
+      for(var key in response) {
+        if(response.hasOwnProperty(key) && key !== 'users') {
+          var itemName = response[key]['name'].replaceBoldWithCase(toReplace, toReplace);
+          var itemUrl = '/' + weeknummer + '/' + response[key]['ref'] + '/' + response[key]['llnr'] + '/';
+          var listItem = '<li><a href="' + itemUrl + '">' + itemName + ' (' + response[key]['llnr'] + ', ' + response[key]['group'] + ')</a></li>';
+          $('ul#acs').append(listItem);
         }
-      });
+      }
+
+      $('ul#acs').children(':first').addClass('selected');
+    } else {
+      $('ul#acs').html('<li class="noresult"><i>Er zijn geen resultaten voor de naam waar je naar zoekt.</i></li>');
+    }
   } else {
     $('ul#acs').html('');
   }
+}
+
+function bothUsers(searchInput) {
+  fetchUsers(searchInput, processUsers);
 }
 
 $(document).ready(function() {
@@ -106,7 +118,7 @@ $(document).ready(function() {
 
     } else if(searchVal !== searchInput.val()) {
       window.clearTimeout(searchTimeout);
-      searchTimeout = window.setTimeout(fetchRoosters, 300, searchInput);
+      searchTimeout = window.setTimeout(bothUsers, 300, searchInput);
       searchVal = searchInput.val();
     }
 
@@ -116,8 +128,7 @@ $(document).ready(function() {
   $('input[type=number]').change(function() {
     weeknummer = $(this).val();
 
-    window.clearTimeout(searchTimeout);
-    searchTimeout = window.setTimeout(fetchRoosters, 300, searchInput);
+    processUsers(searchInput);
   });
 
   $(window).resize(function() {
